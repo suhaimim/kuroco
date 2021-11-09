db1ba0628c3d1427a1acd13944c66a7bc5e69c12
suhaimi <suhaimi.masri@aut-ict.com>
2021-11-09 14:39:52 +0800

    something to do with extension/plugins of vue

--- src/pages/demo/index.vue
+++ src/pages/demo/index.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="container bg-light-grey">
-      <h1>Suhaimi Demo Page</h1>
-  </div>
+    <div class="container bg-light-grey">
+        <h1>Suhaimi Demo Page</h1>
+    </div>
 </template>
 
 <script>
 export default {
 
-}
+};
 </script>
 
 <style>
