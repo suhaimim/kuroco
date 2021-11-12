40e13b3ba68bd990ecd79a0cf717893e6ee624e3
suhaimi <suhaimi.masri@aut-ict.com>
2021-11-13 03:31:04 +0800

    add full layout

---
+++ src/pages/demo/full.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <!-- FOOTER -->
+
+        <footer class="container-fluid bg-dark m-0 py-2">
+            <p class="float-end">
+                <a href="#">Back to top</a>
+            </p>
+            <p class="text-white">&copy; 2017–2021 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+        </footer>
+    </div>
+</template>
+<script>
+export default {
+    layout: 'full'
+};
+</script>
+<style  scoped>
+a {
+    text-decoration: none;
+    color: white;
+}
+</style>
